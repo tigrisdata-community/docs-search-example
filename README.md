38af8c716172e47127f73094a1e8cf99a0fcd399
Xe Iaso <xe@tigrisdata.com>
2025-02-07 20:43:35 +0000

    hard pivot to a search engine
    Signed-off-by: Xe Iaso <xe@tigrisdata.com>

--- README.md
+++ README.md
@@ -1,3 +1,75 @@
-# memegen
+# docs-search-example
 
-Automating social media meme generation from the docs and blog!
+Search through the Tigris documentation and blog with the power of AI!
+
+## Setup
+
+Clone the docs and blog repos.
+
+```text
+cd var
+git clone https://github.com/tigrisdata/tigris-blog
+git clone https://github.com/tigrisdata/tigris-os-docs
+```
+
+### Ingesting data to lancedb
+
+```text
+node ingest.ts
+```
+
+### Searching docs in lancedb
+
+```js
+import * as lancedb from "@lancedb/lancedb";
+import "@lancedb/lancedb/embedding/openai";
+import { LanceSchema, getRegistry } from "@lancedb/lancedb/embedding";
+import { Utf8 } from "apache-arrow";
+
+const func = getRegistry()
+  .get("openai")
+  ?.create({ model: "text-embedding-3-small" });
+
+const contentSchema = LanceSchema({
+  text: func.sourceField(new Utf8()),
+  vector: func.vectorField(),
+  title: new Utf8(),
+  url: new Utf8(),
+});
+
+const tbl = await db.openTable("content", contentSchema);
+
+const query = magic_get_query_somehow(); // fill this in
+
+const actual = await tbl.search(query).limit(25).toArray();
+console.log(`found ${actual.length} results:`);
+
+for (const result of actual) {
+  console.log(`* ${result.title}: ${result.url}\n${result.text}\n`);
+}
+```
+
+### Running the app
+
+Envvars you need:
+
+| Name                    | Description                                                                                 |
+| :---------------------- | :------------------------------------------------------------------------------------------ |
+| `AWS_ACCESS_KEY_ID`     | Tigris access key ID                                                                        |
+| `AWS_SECRET_ACCESS_KEY` | Tigris secret access key                                                                    |
+| `BUCKET_NAME`           | `xe-stream-lancedb`                                                                         |
+| `OPENAI_API_KEY`        | API key for the OpenAI API (needed for generating embeddings with `text-embedding-3-small`) |
+
+Put them in `.env`.
+
+Install the dependencies:
+
+```text
+npm install
+```
+
+Run the server:
+
+```text
+node index.js
+```
--- index.js
+++ index.js
@@ -3,24 +3,12 @@ import * as lancedb from "@lancedb/lancedb";
 import "@lancedb/lancedb/embedding/openai";
 import { LanceSchema, getRegistry } from "@lancedb/lancedb/embedding";
 import { Utf8 } from "apache-arrow";
-import OpenAI from "openai";
-import { zodResponseFormat } from "openai/helpers/zod";
-import { z } from "zod";
 import express from "express";
 import remarkHtml from 'remark-html';
 import remarkParse from 'remark-parse';
 import { unified } from 'unified';
 
-import { loadFiles } from "./utterances.js";
-
-import SupermemeClient from "./web/supermeme.js";
-import { tableFromArrays } from "apache-arrow";
-
-const openai = new OpenAI();
-const model = process.env.OPENAI_MODEL || "gpt-4o";
-const memegen = new SupermemeClient(process.env.SUPERMEME_API_KEY);
-
-const db = await lancedb.connect("s3://xe-stream-lancedb/docs", {
+const db = await lancedb.connect(`s3://${process.env.BUCKET_NAME}/docs`, {
   storageOptions: {
     endpoint: "https://fly.storage.tigris.dev",
     region: "auto",
@@ -40,14 +28,6 @@ const contentSchema = LanceSchema({
 
 const tbl = await db.openTable("content", contentSchema);
 
-const Meme = z.object({
-  topText: z.string(),
-  bottomText: z.string(),
-  // templateName: z.string(),
-});
-
-const utterances = await loadFiles();
-
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -55,80 +35,7 @@ app.use(express.static("static"));
 
 app.set("view engine", "pug");
 
-const getRandomPage = () => {
-  return utterances[Math.floor(Math.random() * utterances.length)];
-};
-
-const getRandomParagraph = (page) => {
-  let result = page.paragraphs[Math.floor(Math.random() * page.paragraphs.length)];
-  return result;
-};
-
-const getMemes = async ({ title, url, paragraph }) => {
-  const input = {
-    title,
-    url,
-    paragraph,
-  };
-
-  console.log(`Generating meme for ${input.title}...`);
-
-  const completion = await openai.beta.chat.completions.parse({
-    model,
-    messages: [
-      {
-        role: "user",
-        content: "You are an expert meme author well known for making the dankest memes of all time, with a preference for obscure memes. Be uplifting and positive. Given the title of the page and contents of a random paragraph, construct a meme prompt and reference the template by name.\n\n" + JSON.stringify({ title, paragraph }),
-      },
-    ],
-    store: true,
-    response_format: zodResponseFormat(Meme, "meme"),
-  });
-
-  const meme = completion.choices[0].message.parsed;
-  console.log(meme);
-  const memeResp = await memegen.textToMeme(`${input.title} ${meme.topText} ${meme.bottomText}`);
-  // console.log(memeResp);
-
-  return {
-    "title": input.title,
-    "url": input.url,
-    "memes": memeResp.memes,
-  };
-};
-
-app.get("/", (req, res) => {
-  res.render("index", { title: "Hey", message: "Hello there!" });
-});
-
-app.get("/meme", async (req, res) => {
-  const memes = await getMemes();
-  res.render("meme", memes);
-});
-
-app.get("/turbomeme", async (req, res) => {
-  const query = req.query.q;
-  console.log(`Searching for ${query}...`);
-  if (!query) {
-    res.text("No query provided, add ?q=your+query to the URL");
-    return;
-  }
-
-  const actual = await tbl.search(query).limit(25).toArray();
-  // shuffle actual
-  actual.sort(() => Math.random() - 0.5);
-  const page = actual[0];
-
-  const memes = await getMemes({
-    title: page.title,
-    url: page.url,
-    paragraph: page.text,
-  });
-
-  res.render("meme", memes);
-});
-
-app.get("/search", async (req, res) => {
+app.get("/", async (req, res) => {
   const query = req.query.q;
   console.log(`Searching for ${query}...`);
   if (!query) {
